e1dea7030614748cd13ffed4bd00da84c0680da2
s0159140 <bert.carlier1@gmail.com>
2015-11-29 13:45:01 +0100

    Update rankHospital.R

--- rankHospital.R
+++ rankHospital.R
@@ -1,22 +1,26 @@
-rankhospital <- function(state, outcome, num = "best") {
-  ## Read outcome data
-  data <- read.csv("./ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
+## rankHospital.R holds a function that takes as arguments a state, a pathology
+## and a certain rank n. It returns as output the n-th ranked hospital in that
+## state for that pathology
+
+rankhospital <- function(state, pathology, num = "best") {
+  ## Read pathology data
+  data <- read.csv("/Users/BertCarlier/Documents/R Working Directory/Coursera-Course-Work/Assignment 3 Hospital Quality/rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
   
-  ## Check that state and outcome are valid
-  validOutcome = c("heart attack","heart failure","pneumonia")
-  if (!outcome %in% validOutcome) { stop("invalid outcome")}
+  ## Check that state and pathology are valid
+  validPathology = c("heart attack","heart failure","pneumonia")
+  if (!pathology %in% validPathology) { stop("invalid pathology")}
   
   validState = unique(data[,7])
   if (!state %in% validState) stop("invalid state")
-    
-  ## convert outcome name into column name
+  
+  ## convert pathology name into column name
   fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
-  colName <- fullColName[match(outcome,validOutcome)]
+  colName <- fullColName[match(pathology,validPathology)]
   
   ## Return hospital name in that state with the given rank 30-day death rate
   data.state <- data[data$State==state,]
   
-  # order data by outcome
+  # order data by pathology
   sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
   
   #handle num input
@@ -25,6 +29,6 @@ rankhospital <- function(state, outcome, num = "best") {
   #will automatically return NA if num > nrow, as well as if it's some other text value
   # if someone passes num < 1, they'll get what's expected
   #if (is.numeric(num) & num > nrwo(sorted.data.state) return(NA)
-    
+  
   sorted.data.state[num,"Hospital.Name"]
 }
