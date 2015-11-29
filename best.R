b83c088163e1d21c1646798537c040cd10b98380
s0159140 <bert.carlier1@gmail.com>
2015-11-29 13:49:54 +0100

    Update best.R

--- best.R
+++ best.R
@@ -1,54 +1,25 @@
-## best.R is a function that takes as arguments the state and the outcome of their care programs
-## for a number of diseases. The function returns the hospital that has the lowest mortality
-## rates in a given state and for a certain pathology (disease)
+## best.R is a function that takes as arguments the state and a certain pathology. 
+## The function returns the hospital that has the lowest mortality rates in a given state 
+## and for a certain pathology
 
-best <- function(state, outcome) {
-	#read in the desired data
-	data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-	
-	#check if the state and outcomes are valid
-	states <- data[ , 7]
-	outcomes <- c("heart attack", "heart failure", "pneumonia")
-	if ((state %in% states) == FALSE) {
-				stop(print("invalid state"))
-	}
-	else if ((outcome %in% outcomes) == FALSE) {
-				stop(print("invalid outcome"))
-	}
+best <- function(state, pathology) {
+  
+  ## Read outcome/pathology data
+   data <- read.csv("/Users/BertCarlier/Documents/R Working Directory/Coursera-Course-Work/Assignment 3 Hospital Quality/rprog-data-ProgAssignment3-data/outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
 
-	#get the subset of the data with the desired state
-	new_data <- subset(data, State == state)
-
-	#get the desired outcome column from the data file
-	if (outcome == "heart attack") {
-				outcome_column <- 11
-	}
-	else if (outcome == "heart failure") {
-				outcome_column <- 17
-	}
-	else {
-				outcome_column <- 23
-	}
-
-	#get rid of the NA's in the desired outcome column
-	required_columns <- as.numeric(new_data[,outcome_column])
-	bad <- is.na(required_columns)
-	desired_data <- new_data[!bad, ]
-
-	#find the hospitals in the rows with the minimum outcome value
-	columns_considered <- as.numeric(desired_data[, outcome_column])
-	desired_rows <- which(columns_considered == min(columns_considered))
-	desired_hospitals <- desired_data[desired_rows, 2]
-
-	#if there are multiple hospitals with the minimum outcome value, then
-	#return the first hospital name from the alphabetically sorted hospital
-	#names list
-
-	if (length(desired_hospitals) > 1) {
-				hospitals_sorted <- sort(desired_hospitals)
-				hospitals_sorted[1]
-	}
-	else {
-				desired_hospitals
-	}
+  ## Check that state and pathology are valid
+  validPathology = c("heart attack","heart failure","pneumonia")
+  if (!pathology %in% validPathology) { stop("invalid pathology")}
+  
+  validState = unique(data[,7])
+  if (!state %in% validState) stop("invalid state")
+  
+  ## convert pathology name into column name
+  fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
+  colName <- fullColName[match(pathology,validPathology)]
+  
+  ## Return hospital name in that state with lowest 30-day death rate
+  data.state <- data[data$State==state,]
+  idx <- which.min(as.double(data.state[,colName]))
+  data.state[idx,"Hospital.Name"]
 }
